853d1b19f11f5638fd2369b994b27395e62c66cb
gigakoxprogramer <sweetgrove12@gmail.com>
2023-02-28 04:36:35 +0100

    Stage 4
    Impemented a not yet reversed mirrored version of the animals with whitespace formatting

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package asciimirror;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,12 +11,28 @@ public class Main {
         Scanner scanner2 = new Scanner(System.in);
         String path = scanner2.nextLine();
         File file = new File(path);
+        int longest = 0;
+        ArrayList<String> list = new ArrayList<>();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNext()) {
-                System.out.println(scanner.nextLine());
+                String line = scanner.nextLine();
+                list.add(line);
+                if (line.length() > longest) {
+                    longest = line.length();
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
+        }
+        for (String line : list) {
+            int diffirence = longest - line.length();
+            StringBuilder str = new StringBuilder(line);
+            while (diffirence > 0) {
+                str.append(" ");
+                diffirence--;
+            }
+            line = str.toString();
+            System.out.print(line + " | " + line + "\n");
         }
     }
 }
